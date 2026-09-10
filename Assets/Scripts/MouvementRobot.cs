14a907f7b0cc0a9aa06f4430b270f65fbdb49ab1
Manare Alouache <2289804@cegepmv.ca>
2026-09-10 16:02:09 -0400

    Relier mouvement animator

--- Assets/Scripts/MouvementRobot.cs
+++ Assets/Scripts/MouvementRobot.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class MouvementRobot : MonoBehaviour
 {
     [SerializeField] private float vitesse = 5f;
 
     private Rigidbody2D corps;
     private Vector2 direction;
+    private Animator animator;
 
     private void Awake()
     {
         corps = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -20,6 +23,7 @@ public class MouvementRobot : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         // TODO 2 : créer et normaliser le vecteur direction.
         direction = new Vector2(horizontal, vertical).normalized;
+        animator.SetBool("EnMouvement", direction.sqrMagnitude > 0.01f);
     }
 
     private void FixedUpdate()
